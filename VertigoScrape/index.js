8381ba05a621b905063ff092755114e4e4fef92d
Loc-ed <41115064+loc-ed@users.noreply.github.com>
2023-02-06 10:42:08 +0200

    removing express server

--- VertigoScrape/index.js
+++ VertigoScrape/index.js
@@ -2,18 +2,21 @@ import initiateScrape from './shoeScrape'
 import initiateHardwareScrape from './hardwareScrape'
 
 const PORT = 8000
-import express from 'express'
+// import express from 'express'
 import chalk from 'chalk'
 
 const button = document.querySelector('.glowing-btn');
 
 function initiateSequence() {
-    const app = express()
-    var server = app.listen(PORT, async function () {
-        console.log(`server running on port ${PORT} `)
-        initiateScrape()
-        initiateHardwareScrape()
-    })
+
+    initiateScrape()
+    initiateHardwareScrape()
+    // const app = express()
+    // var server = app.listen(PORT, async function () {
+    //     console.log(`server running on port ${PORT} `)
+    //     initiateScrape()
+    //     initiateHardwareScrape()
+    // })
 }
 
 button.addEventListener('click', initiateScrape)
