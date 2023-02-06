465d0a4efb7f82bd7fd470234282c997bccf4786
Loc-ed <41115064+loc-ed@users.noreply.github.com>
2023-02-06 10:21:15 +0200

    uncaught reference fix

--- VertigoScrape/index.js
+++ VertigoScrape/index.js
@@ -2,9 +2,10 @@ import initiateScrape from './shoeScrape'
 import initiateHardwareScrape from './hardwareScrape'
 
 const PORT = 8000
-// const button = document.querySelector("#myButton");
 import express from 'express'
 import chalk from 'chalk'
+
+const button = document.querySelector('.glowing-btn');
 
 function initiateSequence() {
     const app = express()
@@ -14,6 +15,10 @@ function initiateSequence() {
         initiateHardwareScrape()
     })
 }
+
+button.addEventListener('click', initiateScrape)
+
+
 // const app = express()
 // var server = app.listen(PORT, async function () {
 //     console.log(`server running on port ${PORT} `)
